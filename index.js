4bccdd0a250dced6508f783c8b784f67a54cdaf2
Ian Lunderskov <ian@lundersaur.us>
2023-03-31 21:26:54 -0500

    Update 'add' button to not reset page and add line containing field inputs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// your code goes here ...
+const addButton = document.getElementsByClassName('add')[0]
+addButton.type = 'button'; // Add type='button' to button to prevent submit and page refresh behavior
+addButton.addEventListener("click", addFamilyMember)
+
+function addFamilyMember() {
+  const relation = document.getElementById('rel').value;
+  const age = document.getElementById('age').value;
+  const smoker = document.getElementById('smoker').checked ? "(smoker)" : "(non-smoker)";
+
+  const householdMember = document.createElement('li')
+  householdMember.textContent = `${relation} ${age} ${smoker}`
+
+  document.getElementsByClassName('household')[0].appendChild(householdMember);
+}
