fb0ebb16271b5d05d23184a58a37a74785ede176
Ian Lunderskov <ian@lundersaur.us>
2023-03-31 22:17:21 -0500

    Disable 'add' button until input validations are met

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const ageInput = document.getElementById('age');
 ageInput.type = 'number';
 ageInput.min = 1;
 
+const relationshipInput = document.getElementById('rel');
+
 const addButton = document.getElementsByClassName('add')[0]
 addButton.type = 'button'; // Add type='button' to button to prevent submit and page refresh behavior
+addButton.disabled = true;
 addButton.addEventListener("click", addFamilyMember)
 
 function addFamilyMember() {
@@ -15,4 +18,18 @@ function addFamilyMember() {
   householdMember.textContent = `${relation} ${age} ${smoker}`
 
   document.getElementsByClassName('household')[0].appendChild(householdMember);
+}
+
+const formElement = document.getElementsByTagName('form')[0];
+formElement.addEventListener("change", validateButtonState);
+
+function validateButtonState() {
+  if (!(ageInput.value > 0)) {
+    addButton.disabled = true;
+  }
+  else if (!(relationshipInput.value)) {
+    addButton.disabled = true;
+  } else {
+    addButton.disabled = false;
+  }
 }
